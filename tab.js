b9f9488477031b06bfab065ba42e0af761b94b39
xy <1425662757@qq.com>
2021-04-05 16:36:08 +0800

    tab

--- tab.js
+++ tab.js
@@ -1,10 +1,6 @@
-# js-component-tab
-## 组件功能
-tab切换
-## 组件实现方式
-```
 function Tab(node){
     this.node = node
+
 }
 Tab.prototype = {
     constructor : Tab,
@@ -30,26 +26,7 @@ Tab.prototype = {
         })
     }
 }
-```
-通过遍历tab-nav操作对应的dom来实现tab切换，如果使用jquery的话则不需要遍历但是每次使用都需要引入jQuery很麻烦所以使用了原生js
-## 如何使用
-将需要操作的dom节点作为参数创建实例
-```
-var tab =  new Tab(document.quearySelector('.tab'))
-```
-需要把用于绑定点击事件控制切换的按钮class设置为`tab-button`  
-同时用于tab切换的区域class设置为.tab  
-且容器内button和tab的数量需要保存一致
-```
-    <div class="tab-container">
-        <div class="nav">
-            <div class="tab-button">TBA1</div>
-            <div class="tab-button">TBA2</div>
-            <div class="tab-button">TBA3</div>
-        </div>
-        <div class="tab">1</div>
-        <div class="tab" >2</div>
-        <div class="tab" >3</div>
-    </div>
-```
-
+var tab1 =  new Tab(document.querySelectorAll('.tab-container')[0]);
+tab1.init();
+var tab2 = new Tab(document.querySelectorAll('.tab-container')[1]);
+tab2.init();
